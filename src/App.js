52670d55dc46a34b941f5110f91962d7a79e9d34
hyojin jo <mlhj6010@naver.com>
2022-08-01 23:01:57 +0900

    [hyojin_jo] array copy 사본 만들기 / sort array 정렬

--- src/App.js
+++ src/App.js
@@ -21,7 +21,7 @@ function App() {
     console.log('bye')
   };
   
-  let [글제목, b] = useState(['여자 코트 추천','강남 우동맛집','파이썬독학']);
+  let [글제목, 제목변경] = useState(['남자 코트 추천','강남 우동맛집','파이썬독학']);
   let [따봉, 따봉변경] = useState(0);
   // state를 써야 하는 이유는
   // state를 써야 하는 이유는
@@ -47,8 +47,43 @@ function App() {
         </div>
         <p>소제목이다</p>
       </div>
+
+      {/* 비효율적인 코드 작성법  */}
+      {/* <span onClick={() => {
+        제목변경(['여자 코트 추천','강남 우동맛집','파이썬독학']);
+      }}>change</span> */}
+
+      {/* 일반적인 코드 작성법 */}
+      {/* <span onClick={() => {
+        글제목[0] = '여자코트 추천'
+        제목변경(글제목);
+      }}>change</span>  */}
+
+      {/* 관습적인 좋은 코드 작성법 */}
+      {/* array로 최초로 저장한것은 남겨두고 따로 array를 카피해서 변경한다. */}
+
+      {/* <span onClick={() => {
+        let copy = 글제목;
+        copy[0] = '여자코트 추천';
+        제목변경(copy);
+      }}>change</span>  */}
+      {/* 이거 왜 안돼 */}
+
+      <span onClick={() => {
+        let copy = [...글제목];
+        copy[0] = '여자코트 추천';
+        제목변경(copy);
+      }}>change</span> 
+
+      <button onClick={() => {
+        let copy = [...글제목];
+        copy.sort();
+        제목변경(copy);
+      }}>가나다순정렬</button>
+
       <div className="list">
-        <h3>{글제목[0]} <span onClick={()=>{따봉변경(따봉+1)}}>❤</span>{ 따봉}</h3>
+        <h3>{글제목[0]}
+          <span onClick={() => { 따봉변경(따봉 + 1) }}>❤</span>{따봉}</h3>
         <p>글 내용이다.</p>
       </div>
       <div className="list">
